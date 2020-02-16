2c35cc16939e0c711c6c1e2bea43522803e5dccd
Samuel Schlesinger <samuel@simspace.com>
2020-02-16 16:56:18 -0500

    made reaper a bit clearer

--- src/UserSystem/Server/Reaper.hs
+++ src/UserSystem/Server/Reaper.hs
@@ -7,32 +7,34 @@ import UserSystem.Database
 import Control.Exception
 import Control.Monad.Reader
 
-data SessionReaper = SessionReaper ThreadId (TVar Bool) (TVar Bool)
+data SessionReaper = SessionReaper ThreadId (TVar SessionReaperState)
+
+data SessionReaperState = Running | Stopping | Stopped
+  deriving Eq
 
 startSessionReaper :: DatabaseT IO SessionReaper
 startSessionReaper = DatabaseT $ ReaderT \pool -> uninterruptibleMask \restore -> do
-  stopRef <- newTVarIO False
-  finishedRef <- newTVarIO False
+  stateRef <- newTVarIO Running
   let
     go = do
-      handle (\(_ :: SomeException) -> return ()) (restore $ (runReaderT . unDatabaseT) (reapSessions' stopRef) pool)
+      handle (\(_ :: SomeException) -> return ()) (restore $ (runReaderT . unDatabaseT) (reapSessions' stateRef) pool)
       test <- atomically $ do
-        stopNow <- readTVar stopRef
-        if stopNow then do
-          writeTVar finishedRef True
-          return True
-        else
-          return False
+        readTVar stateRef >>= \case
+          Stopping -> do
+            writeTVar stateRef Stopped
+            return True
+          Running -> return False
+          Stopped -> error "impossible"
       if test then return () else go
   pid <- forkIO go
-  return $ SessionReaper pid stopRef finishedRef
+  return $ SessionReaper pid stateRef
   where
-    reapSessions' stopRef = do
+    reapSessions' stateRef = do
       reapSessions
-      void $ liftIO (race (threadDelay 10000000) (atomically $ check =<< readTVar stopRef))
+      void $ liftIO (race (threadDelay 10000000) (atomically $ check =<< ((== Stopping) <$> readTVar stateRef)))
 
 stopSessionReaper :: SessionReaper -> IO ()
-stopSessionReaper (SessionReaper pid stop finished) = do
-  atomically $ writeTVar stop True
-  atomically $ check =<< readTVar finished
+stopSessionReaper (SessionReaper pid stateRef) = do
+  atomically $ writeTVar stateRef Stopping
+  atomically $ check =<< ((== Stopped) <$> readTVar stateRef)
   killThread pid
